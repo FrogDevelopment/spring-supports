3c2e3cdba1417994ce326834261ecbdda53716b9
FrogDevelopper <legall.benoit@gmail.com>
2020-10-16 21:45:28 +0200

    feat(cors): add CorsFilter

--- gateway/src/main/java/com/frogdevelopment/gateway/Application.java
+++ gateway/src/main/java/com/frogdevelopment/gateway/Application.java
@@ -1,9 +1,17 @@
 package com.frogdevelopment.gateway;
 
+import java.util.List;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -13,4 +21,16 @@ public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).run();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+
 }
