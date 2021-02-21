ba4d9082e727971850e696f51cf64cce1f700016
FrogDevelopper <legall.benoit@gmail.com>
2021-02-21 20:46:17 +0100

    fix(cors)

--- gateway/src/main/java/com/frogdevelopment/gateway/Application.java
+++ gateway/src/main/java/com/frogdevelopment/gateway/Application.java
@@ -3,12 +3,6 @@ package com.frogdevelopment.gateway;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -16,17 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).run();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        var configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return new CorsFilter(source);
     }
 
 }
